da27eda586d6004afa33a3e23b6c5c6f1e862252
aljorgevi <jormencar@yahoo.com>
2022-06-02 09:33:53 +0100

    set up

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const app = require('./app');
 const http = require('http');
 const config = require('./utils/config');
+const loggers = require('./utils/loggers');
 
 const server = http.createServer(app);
 
 server.listen(config.PORT, () => {
-	console.log(`Server is running on port ${config.PORT}`);
+	loggers.info(`Server is running on port ${config.PORT}`);
+	loggers.info(`visit http://localhost:${config.PORT}`);
 });
