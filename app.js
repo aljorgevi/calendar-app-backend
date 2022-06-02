a01c1aca2708cf910ab2c454dc60e70cb16e0f89
aljorgevi <jormencar@yahoo.com>
2022-06-02 10:03:20 +0100

    add public

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const logger = require('./utils/loggers');
 
 logger.info('connecting to...');
 
-// TODO: move route to separate file
-app.get('/', (req, res) => {
-	console.log('request received');
-	res.json({
-		message: 'Hello World!'
-	});
-});
+// public
+app.use(express.static('public'));
+
+// // TODO: move route to separate file
+// app.get('/', (req, res) => {
+// 	console.log('request received');
+// 	res.json({
+// 		message: 'Hello World!'
+// 	});
+// });
 
 module.exports = app;
