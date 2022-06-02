2e0817a941dd547da9e554a02c69f95725aa10c2
aljorgevi <jormencar@yahoo.com>
2022-06-02 11:55:40 +0100

    add auth route

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 const logger = require('./utils/loggers');
+const authRouter = require('./routes/controllers/auth');
 
 logger.info('connecting to...');
 
 // public
 app.use(express.static('public'));
 
+app.use('/api/v1/auth', authRouter);
+
 // // TODO: move route to separate file
+// TODO: auth, validation, etc. login, etc
+//  CRUD: events
 // app.get('/', (req, res) => {
 // 	console.log('request received');
 // 	res.json({
