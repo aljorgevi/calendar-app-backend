8afa50db4c1ea68b1b59fac9154ef1ee4f665b3c
aljorgevi <jormencar@yahoo.com>
2022-06-02 19:34:09 +0100

    connecting db

--- app.js
+++ app.js
@@ -13,9 +13,7 @@ logger.info('connecting to', config.MONGODB_URI);
 mongoose
 	.connect(config.MONGODB_URI, {
 		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useFindAndModify: false,
-		useCreateIndex: true
+		useUnifiedTopology: true
 	})
 	.then(() => {
 		logger.info('connected to MongoDB');
