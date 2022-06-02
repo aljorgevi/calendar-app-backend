4b45b90d4168bf755a9fa83da2b38cac41c16e0c
aljorgevi <jormencar@yahoo.com>
2022-06-02 09:38:05 +0100

    add logger and port

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const app = require('./app');
 const http = require('http');
 const config = require('./utils/config');
+const logger = require('./utils/loggers');
 
 const server = http.createServer(app);
 
 server.listen(config.PORT, () => {
-	console.log(`Server is running on port ${config.PORT}`);
+	logger.info(`Server is running on port ${config.PORT}`);
+	logger.info(`visit http://localhost:${config.PORT}`);
 });
---
+++ utils/loggers.js
@@ -0,0 +1,16 @@
+const info = (...props) => {
+	if (process.env.NODE_ENV !== 'test') {
+		console.log(...props);
+	}
+};
+
+const error = (...props) => {
+	if (process.env.NODE_ENV !== 'test') {
+		console.error(...props);
+	}
+};
+
+module.exports = {
+	info,
+	error
+};
