68aa89aad913d7bf25b029d0e0aa63fbda47a1b6
aljorgevi <jormencar@yahoo.com>
2022-06-02 09:21:46 +0100

    add utils and app.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log('hello world')
-//
+const app = require('./app');
+const http = require('http');
+const config = require('./utils/config');
+
+const server = http.createServer(app);
+
+server.listen(config.PORT, () => {
+	console.log(`Server is running on port ${config.PORT}`);
+});
