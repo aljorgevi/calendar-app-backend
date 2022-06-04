ba7c0390745c0b20b1b28dcec2a0d3c671480a8f
aljorgevi <jormencar@yahoo.com>
2022-06-04 10:37:58 +0100

    add error handler

--- utils/middlewares.js
+++ utils/middlewares.js
@@ -35,7 +35,9 @@ const errorHandler = (error, request, response, next) => {
 		});
 	}
 
-	logger.error(error.message);
+	logger.error('error.message: ', error.message);
+
+	response.status(400).json({ error: error.message });
 
 	next(error);
 };
