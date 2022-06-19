afef8a853e909503ec4b1ebf4804e2a1e8d9c8db
aljorgevi <jormencar@yahoo.com>
2022-06-19 10:51:59 +0100

    add fllag for test env to show logs

--- utils/loggers.js
+++ utils/loggers.js
@@ -1,11 +1,15 @@
+let ShouldTestEnvHaveLogger = process.env.NODE_ENV !== 'test';
+/* comment next line if logger is not req in test env */
+ShouldTestEnvHaveLogger = true;
+
 const info = (...props) => {
-	if (process.env.NODE_ENV !== 'test') {
+	if (ShouldTestEnvHaveLogger) {
 		console.log(...props);
 	}
 };
 
 const error = (...props) => {
-	if (process.env.NODE_ENV !== 'test') {
+	if (ShouldTestEnvHaveLogger) {
 		console.error(...props);
 	}
 };
